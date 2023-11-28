7ba86d096794ce0d42a6d9ea8270567ed4959dba
Danny Guinther <dannyguinther@gmail.com>
2023-11-28 09:33:40 -0500

    Don't import all workers

--- qless/workers/__init__.py
+++ qless/workers/__init__.py
@@ -1,12 +1,6 @@
-from qless.workers.forking import ForkingWorker
-from qless.workers.greenlet import GeventWorker
-from qless.workers.serial import SerialWorker
 from qless.workers.worker import Worker
 
 
 __all__ = [
-    "ForkingWorker",
-    "GeventWorker",
-    "SerialWorker",
     "Worker",
 ]
