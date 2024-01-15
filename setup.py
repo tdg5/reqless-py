a0fb08a7bb80faf6c48d67e98baa04a8e92ce98b
Danny Guinther <dannyguinther@gmail.com>
2024-01-15 10:14:08 -0500

    Get off funky black beta

--- setup.py
+++ setup.py
@@ -22,14 +22,13 @@ _dependencies = [
 ]
 
 _dev_dependencies = [
-    "black==21.5b2",
+    "black==23.12.1",
     "build==1.0.3",
-    "click<8.1",
     "isort==5.8.0",
     "mypy~=1.7.0",
     "pre-commit==2.20.0",
     "removestar==1.3.1",
-    "safety==2.3.5",
+    "safety==2.3.4",
     "twine==4.0.2",
     "types-decorator==5.1.8.4",
     "types-mock==5.1.0.2",
