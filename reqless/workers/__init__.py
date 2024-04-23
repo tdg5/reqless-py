dcc754f4cf566f064fe6fb87125de2a89c50583d
Danny Guinther <dannyguinther@gmail.com>
2024-04-23 09:28:22 -0400

    Expose workers depending on standard python through reqless.workers

--- reqless/workers/__init__.py
+++ reqless/workers/__init__.py
@@ -1,6 +1,12 @@
 from reqless.workers.base_worker import BaseWorker
+from reqless.workers.forking_worker import ForkingWorker
+from reqless.workers.main_worker import MainWorker
+from reqless.workers.serial_worker import SerialWorker
 
 
 __all__ = [
     "BaseWorker",
+    "ForkingWorker",
+    "MainWorker",
+    "SerialWorker",
 ]
